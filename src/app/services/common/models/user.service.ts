0d23b5d6ab9351428f06f9f29f90b95372961785
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-09-17 10:57:14 +0300

    jwt

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClientService } from '../http-client.service';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { Create_User_Response } from 'src/app/contracts/users/create_user_response';
-import { firstValueFrom } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
+import { AlertifyMessageType, AlertifyPosition, AlertifyService } from '../alertify.service';
+import { Token } from 'src/app/contracts/token/token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private alertify: AlertifyService) { }
 
   async create(user: Create_User): Promise<Create_User_Response> {
     const observable = this.httpClientService.post<Create_User_Response | Create_User>({
@@ -18,13 +20,14 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User_Response;
   }
   async login(email: string, password: string, callback?: () => void) {
-
     try {
-      const observable = this.httpClientService.post({
+      const observable: Observable<any | Token> = this.httpClientService.post<any | Token>({
         controller: "users",
         action: "login"
       }, { email, password })
-      await firstValueFrom(observable)
+      const token: Token = await firstValueFrom(observable) as Token;
+      if (token)
+        localStorage.setItem("accessToken", token.accessToken)
       if (callback) {
         callback();
       }
